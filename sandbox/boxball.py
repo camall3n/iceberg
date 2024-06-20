60a487f97eeb39925f7aed49044cce55d6dce78d
Cam Allen <camallen@berkeley.edu>
2024-06-20 13:25:29 -0700

    Update boxball example

--- sandbox/boxball.py
+++ sandbox/boxball.py
@@ -1,7 +1,7 @@
 import iceberg as ice
 
-rect = ice.Rectangle(ice.Bounds.from_size(64, 64), fill_color=ice.Colors.CYAN)
+box = ice.Rectangle(ice.Bounds.from_size(64, 64), fill_color=ice.Colors.CYAN)
 ball = ice.Ellipse(rectangle=ice.Bounds.from_size(64, 64), fill_color=ice.Colors.YELLOW).background(ice.Colors.BLACK)
-boxball = ice.Arrange(rect, ball, arrange_direction=ice.Arrange.Direction.HORIZONTAL).scale(10,10)
-boxball.render('boxball.png')
+boxball = ice.Arrange(box, ball, arrange_direction=ice.Arrange.Direction.HORIZONTAL).scale(10,10)
+boxball.render('sandbox/boxball.png')
 boxball
